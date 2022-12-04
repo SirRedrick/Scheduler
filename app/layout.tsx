00e2097a2acac6265d76a2318100b21ffe4f75aa
Mykyta Batrak <mykbatrak@gmail.com>
2022-12-05 00:18:23 +0200

    - Added tailwind - Added base styles - Added custom color sets - Added Work Sans font - Fixed issue with scrolling container generated for each page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,27 @@
-import './globals.css'
+import { Work_Sans } from "@next/font/google";
+import { twMerge } from "tailwind-merge";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+import "../styles/globals.css";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const workSans = Work_Sans({
+  variable: "--font-work-sans",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({ children }: Props) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+    <html
+      lang="en"
+      className={twMerge("h-full antialiased", workSans.variable)}
+    >
       <head />
-      <body>{children}</body>
+      <body className="h-full bg-slate-1 text-slate-12 dark:bg-slate-dark-1 dark:text-slate-dark-12">
+        <main className="isolate h-screen">{children}</main>
+      </body>
     </html>
-  )
+  );
 }
